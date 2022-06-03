40f0e14b2868507dd375e272913cd185a5ad82da
ruben rodriguez <proyecto.rubendariorq@gmail.com>
2022-06-03 10:09:23 -0500

    Agrega pruebas unitarias lista mascota
    Se agregan las pruebas unitarias del servicio y del componente

--- src/app/feature/mascota/components/listar-mascota/listar-mascota.component.spec.ts
+++ src/app/feature/mascota/components/listar-mascota/listar-mascota.component.spec.ts
@@ -2,13 +2,20 @@ import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpService } from '@core/services/http.service';
+import { Mascota } from '@mascota/shared/model/mascota';
 import { MascotaService } from '@mascota/shared/service/mascota.service';
+import { of } from 'rxjs';
 
 import { ListarMascotaComponent } from './listar-mascota.component';
 
 describe('ListarMascotaComponent', () => {
   let component: ListarMascotaComponent;
   let fixture: ComponentFixture<ListarMascotaComponent>;
+  let mascotaService: MascotaService;
+  const listaMascotas: Mascota[] = [
+    new Mascota(1, 'MASC001', 'Manotas', 1, null),
+    new Mascota(2, 'MASC002', 'Princesa', 2, null)
+  ]
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -22,10 +29,20 @@ describe('ListarMascotaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarMascotaComponent);
     component = fixture.componentInstance;
+
+    mascotaService = TestBed.inject(MascotaService);
+    spyOn(mascotaService, 'consultar').and.returnValue(of(listaMascotas));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deberia listar mascotas correctamente', () => {
+    component['listarMascotas']();
+    expect(mascotaService.consultar).toHaveBeenCalled();
+    expect(2).toBeGreaterThanOrEqual(component.mascotas.length);
   });
 });
--- src/app/feature/mascota/shared/service/mascota.service.spec.ts
+++ src/app/feature/mascota/shared/service/mascota.service.spec.ts
@@ -1,11 +1,15 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { HttpService } from '@core/services/http.service';
+import { environment } from 'src/environments/environment';
+import { Mascota } from '../model/mascota';
 
 import { MascotaService } from './mascota.service';
 
 describe('MascotaService', () => {
   let service: MascotaService;
+  let httpMock: HttpTestingController;
+  const apiEndpointMascotas = `${environment.endpoint}/mascota`
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +17,26 @@ describe('MascotaService', () => {
       providers: [MascotaService, HttpService]
     });
     service = TestBed.inject(MascotaService);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deberia listar las mascotas correctamente', () => {
+    const dummyMascotas = [
+      new Mascota(1, 'MASC001', 'Manotas', 1, null),
+      new Mascota(2, 'MASC002', 'Harry', 1, null) 
+    ]
+
+    service.consultar().subscribe(mascotas => {
+      expect(mascotas.length).toBe(2);
+      expect(mascotas).toBe(dummyMascotas);
+    });
+
+    const req = httpMock.expectOne(apiEndpointMascotas);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyMascotas);
   });
 });
